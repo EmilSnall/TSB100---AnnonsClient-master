3a4d12561c5cb857f642473c9fcdca9f1fd3fb3a
Emil <Emil@DESKTOP-O8814GC>
2019-05-06 18:24:32 +0200

    Add project files.

---
+++ AnnonsTjanst/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AnnonsTjanst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            return View(client.HamtaAllaAnnonser());
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Create()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Create(ServiceReference1.Annonser annons)
+        {
+            ViewBag.Message = "Your contact page.";
+            ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+            string result = client.SkapaAnnons(annons);
+            ViewBag.Message = result;
+            return RedirectToAction("Index");
+        }
+    }
+}
